bb1eb35b9b3bfa5f9c5302fa33a00f63ce528f22
Gear_Lara <93931507+NatiBorjas@users.noreply.github.com>
2022-03-24 21:05:24 +0100

    Editing img url

--- app/data/tripulacion.js
+++ app/data/tripulacion.js
@@ -5,21 +5,21 @@ const Tripulacion =
         nombre: "Edward “BarbaNegra” Thatch",
         edad: 32,
         puesto: "Capitan",
-        img: "https://natiborjas.github.io/public/img/barbanegra.png"
+        img: "/public/img/barbanegra.png"
     },
     {
         id: 2,
         nombre: "Edward Kenway",
         edad: 27,
         puesto: "Contramaestre",
-        img: "https://natiborjas.github.io/public/img/kenway.png"
+        img: "/public/img/kenway.png"
     },
     {
         id: 3,
         nombre: "Franky",
         edad: 26,
         puesto: "Carpintero",
-        img: "https://natiborjas.github.io/public/img/franky.png"
+        img: "/public/img/franky.png"
         
     },
     {
@@ -27,28 +27,28 @@ const Tripulacion =
         nombre: "Roronoa Zoro",
         edad: 21,
         puesto: "Grumete",
-        img: "https://natiborjas.github.io/public/img/zoro.png"
+        img: "/public/img/zoro.png"
     },
     {
         id: 5,
         nombre: "Monkey D. luffy",
         edad: 18,
         puesto:  "Grumete",
-        img: "https://natiborjas.github.io/public/img/luffy.png"
+        img: "/public/img/luffy.png"
     },
     {
         id: 6,
         nombre: "Nico Robin",
         edad: 25,
         puesto: "Grumete",
-        img: "https://natiborjas.github.io/public/img/robin.png"
+        img: "/public/img/robin.png"
     },
     {
         id: 7,
         nombre: "Anne Bonny",
         edad: 23,
         puesto: "Grumete",
-        img: "https://natiborjas.github.io/public/img/bonny.png"
+        img: "/public/img/bonny.png"
     }
 ]
 
